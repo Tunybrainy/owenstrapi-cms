aef7045a333cea255ecafcd9e3d8f7fa748b65fa
unknown <Tunybrainy>
2025-07-26 17:37:54 +0100

    first commit

---
+++ dist/config/database.js
@@ -0,0 +1,39 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
+exports.default = ({ env }) => {
+    const client = env('DATABASE_CLIENT', 'postgres'); // Default to PostgreSQL for Strapi Cloud
+    const connections = {
+        postgres: {
+            connection: {
+                connectionString: env('DATABASE_URL'), // Strapi Cloud provides this
+                host: env('DATABASE_HOST', 'localhost'),
+                port: env.int('DATABASE_PORT', 5432),
+                database: env('DATABASE_NAME', 'strapi'),
+                user: env('DATABASE_USERNAME', 'strapi'),
+                password: env('DATABASE_PASSWORD', 'strapi'),
+                ssl: env.bool('DATABASE_SSL', true) && {
+                    rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false), // Strapi Cloud may not require strict SSL
+                },
+                schema: env('DATABASE_SCHEMA', 'public'),
+            },
+            pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+        },
+        sqlite: {
+            connection: {
+                filename: path_1.default.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+            },
+            useNullAsDefault: true,
+        },
+    };
+    return {
+        connection: {
+            client,
+            ...connections[client],
+            acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+        },
+    };
+};
